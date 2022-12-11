c83ffb7b55955af5b488ff4492becb6ceab47404
Santeri Valo <valosanteri@gmail.com>
2022-12-11 07:30:32 +0200

    v2 modified working version

--- wordsCounter.c
+++ wordsCounter.c
@@ -1,22 +1,26 @@
 /* Words counter from the terminal */
 
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-#define PLACESIZE 1000
+#define PLACESIZE 10000
 
-int main(void) {
-    int amount = 0;
-    char placeForText[PLACESIZE];
 
-    printf("Insert your text:\n");
-    scanf("%s\n", placeForText);
-    
-    /*
-    for(;;){
-        ++amount; 
-    }
-    */
+int main() {
+     char placeForText[PLACESIZE];
+     int counter = 0;
 
-    printf("Amount of words in your text: %d\n", amount);
-    return 0;
-}
+     printf("Insert your text:\n");
+     fgets(placeForText, PLACESIZE, stdin);
+
+     char* symbol = strtok(placeForText, " ");
+     while (symbol != NULL) {
+          counter++;
+          symbol = strtok(NULL, " ");
+          }
+
+     printf("Amount of words in your text: %d\n", counter);
+     
+     return 0;
+     }
